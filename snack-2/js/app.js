6f7f6ac2126a10718956f48d7dfed269caebac10
lorenzosalti <lorenzosalti@gmail.com>
2025-03-09 08:55:12 +0100

    feat: snack 2 add random number function

--- snack-2/js/app.js
+++ snack-2/js/app.js
@@ -26,3 +26,15 @@ const teams = [
     points: 0
   }
 ];
+
+
+
+
+
+
+
+
+// genera un numero intero random tra min e max inclusi
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
